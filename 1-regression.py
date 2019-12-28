fa663258fdf071c469132f6c2b55ae8a90e08ff7
igofed <i_fedorov@bk.ru>
2019-12-28 08:39:29 +0100

    first download

--- 1-regression.py
+++ 1-regression.py
@@ -1,8 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Dec 27 20:30:37 2019
-
-@author: igofed
-"""
-
 import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+import tensorflow as tf
+import matplotlib.patches as mpatches
+#plt.rcParams['figure.figsize'] = (10,6)
+plt.figure()
+X = np.arange(0.0, 5.0, 0.1)
+a = 1
+b = 0
+Y = a * X + b
+plt.plot(X, Y)
+plt.grid(True)
+plt.ylabel('Dependent Variable')
+plt.xlabel('InDependent Variable')
+plt.show()
